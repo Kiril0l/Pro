92ef43ac60f2e3ade4e090bb6bc29d4710de966d
Kiril0l <Kirill_1998_12@mail.ru>
2019-07-23 21:17:19 +0300

    BOT

--- BOT/weather.py
+++ BOT/weather.py
@@ -1,33 +1,59 @@
+import requests
+from datetime import datetime
+import locale
 API_KEY ="d52fbaedd1ca6f5f423a54a5a5e7374d"
 URL = "https://api.openweathermap.org/data/2.5/weather"
 
 class Weather():
     def __init__(self, *args, **kwargs):
         if kwargs.get("country") and kwargs.get("city"):
-            self.url = "{0}?{1}&appid={2}".format(URL, f"q={kwargs['city']},{kwargs['country']}",API_KEY)
+            self.url = "{0}?{1}&appid={2}&units=metric&lang=ru".format(URL, f"q={kwargs['city']},{kwargs['country']}",API_KEY)
         elif kwargs.get("latitude") and kwargs.get("longitude"):
-            self.url = "{0}?{1}&appid={2}".format(URL,            f"lat={kwargs['latitude']},&lon={kwargs['longitude']}",API_KEY)
+            self.url = "{0}?{1}&appid={2}&units=metric&lang=ru".format(URL,            f"lat={kwargs['latitude']},&lon={kwargs['longitude']}",API_KEY)
         else:
-            # London, UK
+            self.url = "{0}?{1}&appid={2}&units=metric&lang=ru".format(URL, f"q=London,UK",API_KEY)
             pass
 
 
     def get_date(self):
-        pass
+        response = requests.get(self.url)
+        if response.status_code == 200:
+            self.result = response.json()
+        else:
+            self.result = {}
 
 
     def send_db(self):
         pass
 
 
+
     def create_response(self):
-        return None
+        locale.setlocale(locale.LC_TIME, 'ru_RU.UTF-8')
+        template = f"""Сегодня, {datetime.utcfromtimestamp(self.result["dt"] + self.result["timezone"]).strftime('%d %B %Y %H:%M:%S')}.
+    Погода в {self.result["name"]} {self.result["sys"]["country"]}:
+      {self.result["weather"][0]["main"]}, {self.result["weather"][0]["description"]}.
+      Температура воздуха: {self.result["main"]["temp"]}.
+      Атмосферное давление: {self.result["main"]["pressure"]} мм/рс.
+      Влажность воздуха: {self.result["main"]["humidity"]}%.
+      Ветер: {self.result["wind"]["deg"]}.
+      Скорость ветра: {self.result["wind"]["speed"]} м/c.
+    """
+        print(template)
 
     def get_response(self):
-        pass
+        # return self.response
+        return None
+
 
 if __name__ == "__main__":
     obj = Weather(country= "BY", city = "Minsk")
     print(obj.url)
-    obj2 = Weather(latitude=3.54, longitude=-0.28)
-    print(obj2.url)
+    obj.get_date()
+    # print(obj.result)
+    obj.create_response()
+    # obj2 = Weather(latitude=3.54, longitude=-0.28)
+    # print(obj2.url)
+    # obj3 = Weather()
+    # print(obj3.url)
+    # obj3.get_date()
