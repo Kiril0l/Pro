541888fbae78da363115900fe2c5814d11525138
Kiril0l <Kirill_1998_12@mail.ru>
2019-08-27 21:39:14 +0300

    jdfhd

--- transleater/trbot.py
+++ transleater/trbot.py
@@ -1,8 +1,9 @@
 import telebot
-
 bot = telebot.TeleBot("902231828:AAFwNVWjUJFTstm6ef-hH6mjr_OOvUiTKgg")
 
 @bot.message_handler(content_types=['text'])
 def send_echo(message):
-    bot.send_message(message.cgat.id, message.txt)
+    bot.send_message(message.chat.id, message.text)
 bot.polling( none_stop=True)
+
+# pip3 install pyTelegramBotAPI
